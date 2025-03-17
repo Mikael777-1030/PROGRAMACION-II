b34b25a22d35ac8dbb0a525e46c516b527ee6944
Mikael777-1030 <brandel_rosales2020@hotmail.com>
2025-03-17 15:19:18 -0400

    figurageometrica_singledispatch2.py

--- PRACTICA4/figurageometrica_singledispatch2.py
+++ PRACTICA4/figurageometrica_singledispatch2.py
@@ -2,7 +2,7 @@
 from functools import singledispatchmethod
 import math
 
-# Definimos clases para cada figura
+
 class Circulo:
     def __init__(self, radio):
         self.radio = radio
@@ -27,34 +27,34 @@ class PentagonoRegular:
     def __init__(self, lado):
         self.lado = lado
 
-# Clase principal que calcula las áreas
+
 class FiguraGeometrica:
     
     @singledispatchmethod
     def area(self, figura):
         raise ValueError("Figura no soportada")
     
-    # Círculo
+
     @area.register
     def _(self, figura: Circulo):
         return math.pi * figura.radio**2
 
-    # Rectángulo
+    
     @area.register
     def _(self, figura: Rectangulo):
         return figura.base * figura.altura
 
-    # Triángulo
+    
     @area.register
     def _(self, figura: Triangulo):
         return (figura.base * figura.altura) / 2
 
-    # Trapecio
+    
     @area.register
     def _(self, figura: Trapecio):
         return ((figura.base_mayor + figura.base_menor) * figura.altura) / 2
 
-    # Pentágono regular (se calcula el apotema)
+    
     @area.register
     def _(self, figura: PentagonoRegular):
         apotema = figura.lado / (2 * math.tan(math.pi / 5))
