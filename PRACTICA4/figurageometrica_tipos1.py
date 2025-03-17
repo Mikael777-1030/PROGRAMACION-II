5c3a855ff77f74e397553f05b0ed6c8cebe40613
Mikael777-1030 <brandel_rosales2020@hotmail.com>
2025-03-17 15:18:33 -0400

    figurageometrica_tipos1.py

--- PRACTICA4/figurageometrica_tipos1.py
+++ PRACTICA4/figurageometrica_tipos1.py
@@ -6,15 +6,15 @@ import math
 class FiguraGeometrica:
     def area(self, a, b=None, c=None, d=None, e=None):
         if isinstance(a, float) and b is None and c is None and d is None and e is None:
-            return math.pi * a * a # Círculo (radio)
+            return math.pi * a * a
         elif isinstance(a, float) and isinstance(b, float) and c is None and d is None and e is None:
-            return a * b # Rectángulo (base, altura)
+            return a * b 
         elif isinstance(a, float) and isinstance(b, float) and isinstance(c, float) and d is None and e is None:
-            return (a * b) / 2 # Triángulo (base, altura, ignorado)
+            return (a * b) / 2 
         elif isinstance(a, float) and isinstance(b, float) and isinstance(c, float) and isinstance(d, float) and e is None:
-            return (a + b) * c / 2 # Trapecio (base mayor, base menor, altura, ignorado)
+            return (a + b) * c / 2 
         elif isinstance(a, float) and isinstance(b, float) and isinstance(c, float) and isinstance(d, float) and isinstance(e, float):
-            return 5 * a * a / (4 * math.sqrt(5 - 2 * math.sqrt(5))) # Pentágono regular (lado, ignorados ...)
+            return 5 * a * a / (4 * math.sqrt(5 - 2 * math.sqrt(5))) 
 
 figura = FiguraGeometrica()
 print("Área del círculo:", figura.area(5.0))
