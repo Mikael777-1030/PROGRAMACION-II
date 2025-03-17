fa7fa0aabac75fec140284c2db90b677020e0b34
Mikael777-1030 <brandel_rosales2020@hotmail.com>
2025-03-17 15:16:11 -0400

    figurageometrica_multimethod.py

--- PRACTICA4/figurageometrica_multimethod.py
+++ PRACTICA4/figurageometrica_multimethod.py
@@ -7,29 +7,29 @@ import math
 
 class FiguraGeometrica:
     
-    # Círculo: 1 parámetro (radio)
+    
     @multimethod
     def area(self, radio: float):
         return math.pi * radio**2
 
-    # Rectángulo: 2 parámetros (base, altura)
+    
     @multimethod
     def area(self, base: float, altura: float):
         return base * altura
 
-    # Triángulo: 2 parámetros (base, altura)
+    
     @multimethod
     def area(self, base: float, altura: float, tipo: str):
         if tipo == "triangulo":
             return (base * altura) / 2
         raise ValueError("Tipo de figura desconocido")
 
-    # Trapecio: 3 parámetros (base mayor, base menor, altura)
+    
     @multimethod
     def area(self, baseMayor: float, baseMenor: float, altura: float):
         return ((baseMayor + baseMenor) * altura) / 2
 
-    # Pentágono regular: 1 parámetro (lado)
+
     @multimethod
     def area(self, lado: float, tipo: str):
         if tipo == "pentagono":
